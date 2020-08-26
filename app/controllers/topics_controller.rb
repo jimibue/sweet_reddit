06f3175b1a0cf2ce3f6d91fba282c52b008bd18e
James Yeates <jimibue@gmail.com>
2020-08-26 13:43:20 -0700

    done

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -6,7 +6,7 @@ class TopicsController < ApplicationController
 
   def show
     @sub = Sub.find(params[:sub_id])
-    @topic = Topic.find(params[:id])
+    @topic = @sub.topics.find(params[:id])
   end
 
   def new
@@ -16,6 +16,54 @@ class TopicsController < ApplicationController
     render partial: "form"
   end
 
+  def create
+    # get the sub, then use that sub to create a topic
+    @sub = Sub.find(params[:sub_id])
+    @topic = @sub.topics.new(topic_params)
+
+    if @topic.save
+      # redirect_to [@sub, @topic]
+      redirect_to sub_topic_path(@sub, @topic)
+    else
+      render partial: "form"
+    end
+  end
+
   def edit
+    @sub = Sub.find(params[:sub_id])
+    @topic = @sub.topics.find(params[:id])
+
+    render partial: "form"
+  end
+
+  def update
+    @sub = Sub.find(params[:sub_id])
+    @topic = @sub.topics.find(params[:id])
+
+    if (@topic.update(topic_params))
+      redirect_to sub_path(@sub)
+    else
+      render partial: "form"
+    end
+  end
+
+  def destroy
+    # reminder we have params[:sub_id] to get to the sub
+    # have params[:id] to get the topic
+
+    # another way preffered
+    @sub = Sub.find(params[:sub_id])
+
+    topic = @sub.topics.find(params[:id])
+
+    topic.destroy
+
+    redirect_to sub_path(@sub)
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:name, :body)
   end
 end
